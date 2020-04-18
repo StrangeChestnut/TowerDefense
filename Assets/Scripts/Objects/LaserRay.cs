74c90217f7a394f92fffa77c96bf926cabf283b3
StrangeChestnut <strangechestnut@gmail.com>
2020-04-18 14:25:31 +0300

    Created a Magic tower with laser weapon

---
+++ Assets/Scripts/Objects/LaserRay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Objects
+{
+    public class LaserRay : MonoBehaviour
+    {
+        public float damagePerSecond = 5f;
+
+        private void OnTriggerStay(Collider other)
+        {
+            var enemy = other.GetComponentInParent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.Character.Health.Damage(damagePerSecond * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Tower.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Objects;
+using ScriptableObjects;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public GameController game;
+    public Weapon weapon;
+    public float attackDistance = 5f;
+    private Transform _target;
+
+    private void Update()
+    {
+        if (_target != null)
+        {
+            if (InSight(_target.position))
+            {
+                weapon.Rotate(_target.position);
+                weapon.Fire();
+            }
+            else
+                UpdateTarget();
+        }
+        else
+        {
+            weapon.StopFire();
+            UpdateTarget();
+        }
+    }
+
+    private bool InSight(Vector3 target)
+    {
+        var vector = target - transform.position;
+        vector = new Vector2(vector.x, vector.z);
+        return vector.magnitude <= attackDistance;
+    }
+
+    private void UpdateTarget()
+    {
+        var shortestDist = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (var enemy in game.enemies)
+        {
+            var distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < shortestDist)
+            {
+                shortestDist = distance;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && InSight(nearestEnemy.transform.position))
+            _target = nearestEnemy.transform;
+        else
+            _target = null;
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+    }
+}
